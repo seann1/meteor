dc8f684fd3c33978fbddbb2e2d5c06703d11b6cf
Sean Niesen <seanniesen@gmail.com>
2015-06-14 03:29:29 -0700

    going to bed

--- client/views/home.js
+++ client/views/home.js
@@ -6,5 +6,21 @@ Router.route('home', {
 Template.home.helpers({
 	restaurants: function() {
 		return Restaurants.find();
+	},
+	defaultImage: function() {
+		function defaultPic(image) {
+			if (image.defaultPic === true) {
+				return true
+			} else {
+				return false
+			}
+		};
+		return this.images.filter(defaultPic)[0].pic
 	}
 });
+
+Template.home.events({
+	'click .enter-block': function(e, template) {
+		Router.go('/restaurants/'+this._id)
+	}
+})
